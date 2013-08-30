21413ee8445857b5856f8422c50a389b7fe9d768
bmikeb <mikeb@ukr.net>
2013-08-30 20:41:25 +0300

    Update gestures2.js

--- js/gestures2.js
+++ js/gestures2.js
@@ -1,107 +1,151 @@
 //(function($){
 //    $.fn.initGestures= function(){
-function initGestures(arg){
-        var startX = 0, startY= 0, curX = 0, curY = 0
-            ,startTime=0
-            ,MIN_LENGTH_X = 72, MIN_LENGTH_Y = 50
-            ,doubleClick = false, paused = false;
-        swipeLengthX = 0, swipeLengthY = 0;
-//        $this = $(this);
-    $this = arg;
+captionPanelClosed = true
+function transferCaptionInput() {
+}
+function initCaption(e, curX) {
+    evt = e.originalEvent;
+    var Y = e.pageY || evt.changedTouches[0].pageY;
 
-//        return this.each(function(){
-            $this.on('touchstart mousedown', touchStartHandler);
-            $this.on('touchmove mousemove', touchMoveHandler);
-            $this.on('touchend mouseup', touchEndHandler);
 
-            function touchStartHandler(e) {
-                e.preventDefault();
-                startX = e.pageX;
-                startY = e.pageY;
-                if(startTime == 0) {
-                    startTime = new Date();
-                }else {
-                    var delta = new Date() - startTime;
-                    if(delta>75 && delta <300){
-                        doubleClick = true;
-                    }
-                    else if (delta>300){
-                        startTime = new Date();
-                        doubleClick = false;
-
-                    }else{
-                        startTime=0;
-                        doubleClick = false;
-                    }
-                }
-            };
-
-            function touchMoveHandler(e) {
-                e.preventDefault();
-                curX = e.pageX;
-                curY = e.pageY;
-
-                setTimeout(function(){
-                    if( e.pageY > 700 && (e.pageX > curX-10 && e.pageX < curX+10) ){
-                        $('.captionContainer').css({visibility: 'visible'});
-                    }
-                }, 1000);
-                if( e.pageY < 700)
-                    $('.captionContainer').css({visibility: 'hidden'});
-            };
-
-            function touchEndHandler(e, innerNum) {
-                e.preventDefault();
-
-                if ( curX != 0 || curY !=0) {
-                    swipeLengthX = Math.abs(curX - startX);
-                    swipeLengthY = Math.abs(curY - startY);
-                    if ( e.target.className != "caption") {
-                        if(swipeLengthX >= MIN_LENGTH_X
-                            || swipeLengthY >= MIN_LENGTH_Y
-                            && curY > 650){
-
-                            processingRoutine();
-
-                            touchCancelHandler();
-                            return;
-                        }
-                    }
-                }
-                setTimeout(function(){//passing e as event causes error
-                    var tgt = $(e.target);
-                    if(doubleClick && tgt[0].className != "caption") {
-                        if(tgt.attr('src').indexOf('-')!=-1){
-                            tgt.attr({src: "img/"+prefix+ ".jpg"});
-                            ix = prefix;
-                            populateHotAreas($('#slider').children(":first"));
-                            localTrip = false;
-                        }
-                    }else{
-                        curX = curX || startX;
-                        curY = curY || startY;
-                        var e_parent = tgt[0].parentElement;
-                        if ( Math.abs(curX - startX) < 5){
-                            if (e_parent.className=='thumbnail') {
-                                displayBigThumb(e_parent.id);
-                                localTrip = true;
-                            }
-                        }
-                    }
-                    touchCancelHandler(e);
-                }, 300)
-            };
-            function touchCancelHandler(event) {
-                startX = 0, startY = 0;
-                curX = 0, curY = 0;
-            };
-            function processingRoutine() {
-                if ( startX > curX ) {
-                    displaynext(1);
-                }
-                else displaynext(-1);
-            };
-//         return this;
-//        })
+    if (Y > 700 && !swiping && captionPanelClosed) {
+        $('.captionContainer').animate({opacity:0.4}, 1000, handleCaptionText );
     }
+}
+function returnToPages(tgt) {
+    if (tgt.attr('src').indexOf('-') != -1) {
+        tgt.attr({src:"img/" + prefix + ".jpg"});
+        ix = prefix;
+        populateHotAreas($('#slider').children(":first"));
+
+        previewPnl.show();
+        caption.show();
+    }
+}
+function initGestures(/*arg*/){
+    var startX = 0, startY= 0, liveX = 0, liveY = 0 ,startTime=0
+        ,MIN_LENGTH_X = 72, MIN_LENGTH_Y = 50;
+
+    deltaX = 0, deltaY = 0;
+    touchStarted = false;
+    adjusting = false;
+    swiping = false;
+    $this = $('body');
+
+//for iPad version
+//    $this.on('touchstart /*mousedown*/', touchStartHandler);
+//    $this.on('touchmove /*mousemove*/', touchMoveHandler);
+//    $this.on('touchend /*mouseup*/', touchEndHandler);
+
+  $this.on('mousedown', touchStartHandler);
+    $this.on('mousemove', touchMoveHandler);
+    $this.on('mouseup', touchEndHandler);
+
+//    detectHovering();
+
+
+    function touchStartHandler(e) {
+        e.preventDefault();
+        var evt = e.originalEvent;
+
+        startX = e.pageX || evt.changedTouches[0].pageX;
+        startY = e.pageY || evt.changedTouches[0].pageY;
+
+        touchStarted = true;
+        startTime = new Date();
+
+        img = $(".sliderSpot > img");
+        startImgX = img.offset().left, startImgY = img.offset().top;
+        startImgW = img.width(), startImgH = img.height();
+
+        var el = $(evt.target);
+
+        if( /thumbs/.test(el.attr('src')) ){
+            el.parent().trigger('mouseover');
+            origX =startX, origY = startY;
+        }
+    }
+
+    function touchMoveHandler(e) {
+        e.preventDefault();
+        var evt = e.originalEvent;
+
+        liveX = e.pageX || evt.changedTouches[0].pageX;
+        liveY = e.pageY || evt.changedTouches[0].pageY;
+
+        if(touchStarted){
+            deltaX = liveX - startX;
+            deltaY = liveY - startY;
+
+            if( new Date() - startTime>750){
+                adjusting = true;
+/*
+                if(startX<100 && startY<100 || startX>924 && startY>668){//corners only
+                    var ratio = 1+ deltaY/startImgH;
+                    img.css({width:startImgW*ratio+"px", height:startImgH*ratio+"px"});
+                }else{
+                    var accumX = startImgX + deltaX;
+                    var accumY = startImgY + deltaY;
+                    img.css({position:"relative", left: accumX+"px", top:accumY+"px"})
+                }
+*/
+            }
+        }
+    };
+
+    function touchEndHandler(e) {
+        e.preventDefault();
+        var tgt = $(e.target) || $(e.originalEvent.target);
+
+//for desktop only
+       // tgt.trigger('click');
+
+        transferCaptionInput();
+
+        if ( tgt.attr('id') != "undefined" && tgt.attr('id') != "captionContent" ) {
+            console.log(tgt.attr('id'))
+
+            var dX = Math.abs(deltaX), dY = Math.abs(deltaY);
+            if(dX >= MIN_LENGTH_X || dY >= MIN_LENGTH_Y){
+                captionPanelClosed = true
+                doSwipe();
+            }
+            if( Math.abs(deltaX) < 5 ){
+                if(localTrip){
+
+                    returnToPages(tgt);
+
+                    localTrip = false;
+                }else{
+                    var e_parent = tgt[0].parentElement;
+                    if (e_parent.className=='thumbnail') {
+
+//                                saveToStorage(ix);
+                        displayBigThumb(e_parent.id);
+
+                        localTrip = true;
+                    }
+                }
+            }
+        }
+        if( !localTrip)
+            initCaption(e, liveX);///for not to show on localTrip
+
+        touchCancelHandler();
+    };
+    function touchCancelHandler(e) {
+        deltaX = 0;
+        touchStarted = false;
+        adjusting = false;
+        swiping = false;
+    };
+    function doSwipe() {
+        swiping=true;
+        if ( startX > liveX ) {
+            displaynext(1);
+        }
+        else displaynext(-1);
+    };
+//     return this;
+ }
 //})(jQuery);
